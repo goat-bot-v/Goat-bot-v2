54ef1ad4952ea6c7fd51ef8cad18999fc5856938
goat-bot-v <kakashib28@gmail.com>
2025-06-24 14:15:06 +0500

    Update flux.js

--- scripts/cmds/flux.js
+++ scripts/cmds/flux.js
@@ -6,7 +6,7 @@ module.exports.config = {
   role: 0,
   author: "Dipto",
   description: "Flux Image Generator",
-  category: "𝗜𝗠𝗔𝗚𝗘 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗢𝗥",
+  category: "image",
   premium: true,
   guide: "{pn} [prompt] --ratio 1024x1024\n{pn} [prompt]",
   countDown: 15,
